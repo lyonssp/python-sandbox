b8605bccddf01d180fc5fdb1dcf1f8c5fc2e1963
EC2 Default User <ec2-user@ip-172-31-23-187.us-east-2.compute.internal>
2020-04-10 02:37:11 +0000

    I added some stuff about lists about another thing I like... hogs.

--- miki/sugarbabyluvsu.py
+++ miki/sugarbabyluvsu.py
@@ -19,3 +19,25 @@ codes = [5, 4, 3]
 print(codes)
 for language in codes:
     print(language*2)
+hogs = ['javelina', 'feral', 'teacup']
+littlehog = hogs[2]
+print(littlehog)
+for hog in hogs:
+    print("I wish I was a " + hog + " hog")
+    print("I wish I had a " + hog + " hog")
+hogs[2] = 'babe'
+print(hogs)
+hogs.append('teacup')
+print(hogs)
+for hog in hogs:
+    print("Take me down to paradise city where the hogs are " + hog + " and there's 40-50")
+hogs.insert(2, 'farm')
+print(hogs)
+hognames = []
+print(hognames)
+hognames.insert(0,'cronk')
+hognames.insert(1, 'spud')
+hognames.append('groot')
+print(hognames)
+hognames.sort()
+print(hognames)
